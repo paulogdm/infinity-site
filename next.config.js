2870af23f74cd35d82820f8ee9f08fe00a24c683
paulogdm <paulogdemitri@gmail.com>
2019-01-03 15:29:46 -0200

    Now V2 proposal

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withOffline = require('next-offline')
 require('dotenv').config()
 
 module.exports = withOffline({
+  target: 'serverless',
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
     ASSETS_URL: process.env.ASSETS_URL
